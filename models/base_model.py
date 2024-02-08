2055bd3fc71e830b30244724663e11ada795aa27
MostafaAlaa297 <elaskary609@gmail.com>
2024-02-08 10:34:09 -0800

    finish tests and fix issues

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,19 @@ class BaseModel:
     The base class
     """
 
-    def __init__(self, id=None):
+    def __init__(self, **kwargs, ):
         """
         Initialization of the base module
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+    
     def __str__(self):
         """
         Returns info about the model
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,30 +5,20 @@ Test base module
 ================
 """
 
-import unittest
+from datetime import datetime, timedelta
 from models.base_model import BaseModel
+import unittest
 
 class TestBase(unittest.TestCase):
     """
     Test the base class
     """
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.mynumber = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("json of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
     def setUp(self):
         """
         Executes once before each test case
         """
-        pass
+        self.my_model = BaseModel()
 
     def tearDown(self):
         """
@@ -36,11 +26,98 @@ class TestBase(unittest.TestCase):
         """
         pass
 
+    def test_attributes_on_creation(self):
+        """
+        Test attributes on creation
+        """
+        self.assertTrue(hasattr(self.my_model, "id"))
+        self.assertTrue(hasattr(self.my_model, "created_at"))
+        self.assertTrue(hasattr(self.my_model, "updated_at"))
+        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime)
+
+    def test_id_uniqueness(self):
+        """
+        Test id uniqueness
+        """
+        obj = BaseModel()
+        self.assertNotEqual(self.my_model.id, obj.id)
+
+    def test_str_representaion(self):
+        """
+        Test __str__ representation
+        """
+        expected_str = "[BaseModel] ({}) {}".format(self.my_model.id, self.my_model.__dict__)
+        self.assertEqual(str(self.my_model), expected_str)
+
+    def test_save_method(self):
+        """
+        Test save method updates updated_at
+        """
+        old_updated_at = self.my_model.save()
+        self.my_model.save()
+        self.assertNotEqual(old_updated_at, self.my_model.updated_at)
+
+    def test_to_dict_method(self):
+        """
+        Test to_dict method
+        """
+        self.my_model.name = "Test model"
+        self.my_model.my_number = 55
+        obj_dict = self.my_model.to_dict()
+
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict["id"], self.my_model.id)
+        self.assertEqual(obj_dict["created_at"], self.my_model.created_at.isoformat())
+        self.assertEqual(obj_dict["updated_at"], self.my_model.updated_at.isoformat())
+        self.assertEqual(obj_dict["name"], "Test model")
+        self.assertEqual(obj_dict["my_number"], 55)
+
     def test_id_is_not_none(self):
         """
-        Tests if id is not None
+        Tests if id is not none
         """
-        pass
+        self.assertIsNotNone(self.my_model.id, "Test value is none")
+
+    def test_id_is_string(self):
+        """
+        Tests if id is a string
+        """
+        self.assertIsInstance(self.my_model.id, str)
+
+    def test_created_at_type_conversion(self):
+        """
+        Test created_at type conversion to string
+        """
+        created_at_str = self.my_model.to_dict()["created_at"]
+        self.assertIsInstance(created_at_str, str)
+
+    def test_updated_at_type_conversion(self):
+        """
+        Test updated_at type conversion to string
+        """
+        self.my_model.save()
+        updated_at_str = self.my_model.to_dict()["updated_at"]
+        self.assertIsInstance(updated_at_str, str)
+
+    def test_reload_to_dict(self):
+        """
+        Test reloading from the dictionary representaion
+        """
+        self.my_model.name = "Test reloading"
+        self.my_model.save()
+
+        obj_dict = self.my_model.to_dict()
+
+        class_name = obj_dict["__class__"]
+        del obj_dict["__class__"]
+        reloaded_obj = globals()[class_name](**obj_dict)
+
+        self.assertEqual(reloaded_obj.id, self.my_model.id)
+        self.assertEqual(reloaded_obj.name, self.my_model.name)
+        self.assertEqual(reloaded_obj.created_at, self.my_model.created_at)
+        self.assertEqual(reloaded_obj.updated_at, self.my_model.updated_at)
+
 
 if __name__ == "__main__":
     unittest.main()
